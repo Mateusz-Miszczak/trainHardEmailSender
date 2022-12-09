ec3b10a154094989dccc88486a6ab602972c81e3
Mateusz Miszczak <mthw.miszczak@gmail.com>
2022-12-09 20:38:42 +0100

    Change style of a button function, started function that checks times  clicked

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 "use strict";
 
-const allMuscles = document.querySelectorAll(".body-part");
+const allMuscles = document.querySelectorAll(".body-part-btn");
 const containerBodyParts = document.getElementById("body-parts-container");
 const testDiv = document.getElementById("test");
 const testDiv2 = document.getElementById("test2");
+const submitButton = document.getElementById("btn-submit");
 
 console.log(testDiv);
 
@@ -22,11 +23,13 @@ const createInputAndLabelOnClick = () => {
       let takeContent = muscle.textContent;
       createLabel(takeContent);
       createInput(testDiv2);
+      btnPossAfterClick(submitButton);
+      checkTimesClicked();
     });
   });
 };
 
-const createLabel = (contentOfLabel, varToStyle) => {
+const createLabel = (contentOfLabel) => {
   const label = document.createElement("label");
   label.setAttribute("for", "input");
   label.innerHTML = `${contentOfLabel}`;
@@ -46,7 +49,6 @@ const createInput = (varToStyle) => {
   input.setAttribute("type", "text");
   varToStyle.style.display = "grid";
   varToStyle.style.gridTemplateColumns = "25fr 25fr";
-  // varToStyle.style.borderLeft = " 1px solid black";
   varToStyle.style.width = "100%";
   varToStyle.style.height = "20rem";
   varToStyle.appendChild(input);
@@ -55,6 +57,36 @@ const createInput = (varToStyle) => {
 const changeBodyPartMargin = () => {
   containerBodyParts.style.margin = "2rem 2rem";
 };
+
+const btnPossAfterClick = (button) => {
+  button.style.left = "35%";
+};
+
+// TODO
+
+// const objectTimesClicked = {
+//   neck: 0,
+//   frontShoulder: 0,
+//   sideShoulder: 0,
+//   rearShoulder: 0,
+//   biceps: 0,
+//   triceps: 0,
+//   upperForearm: 0,
+//   lowerForearm: 0,
+//   upperChest: 0,
+//   middleChest: 0,
+//   bottomChest: 0,
+// };
+
+// const checkTimesClicked = () => {
+//   allMuscles.forEach((muscle) => {
+//     muscle.addEventListener("click", () => {
+//       objectTimesClicked[muscle.value] += 1;
+//       console.log(objectTimesClicked);
+//       // muscle.innerText = objectTimesClicked[muscle.value];
+//     });
+//   });
+// };
 
 const runApp = () => {
   changeBodyPartMargin();
